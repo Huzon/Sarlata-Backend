36a9429511797a6b67a217f7e50aae6ac98c4507
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2022-04-19 12:34:34 +0400

    error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 //syntax : mongoose.connect('url/db-name', {options})
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose
+  .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     // useCreateIndex: true,
-});
+  })
+  .then(() => console.log("connected"))
+  .catch((e) => console.log(e));
